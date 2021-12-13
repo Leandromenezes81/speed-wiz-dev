4c3d772d73f6e49e5ef77ee30a5e3712f7747d16
Leandromenezes81 <leandromenezes1@gmail.com>
2021-12-13 12:24:55 -0300

    Conclusão Módulo IV.

--- modulo4/projetos/AgendaComDapper/Controllers/ContatosController.cs
+++ modulo4/projetos/AgendaComDapper/Controllers/ContatosController.cs
@@ -2,8 +2,6 @@
 using AgendaComDapper.Models;
 using AgendaComDapper.Repositorio;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
@@ -51,7 +49,63 @@ namespace AgendaComDapper.Controllers
                 return Ok(contato);
 
             return StatusCode(500, "Contato não pode ser adicionado");
-        }       
+        }
+
+        [HttpPut("atualizar-contato-por-telefone")]
+        public async Task<IActionResult> Atualizar(AtualizarContatoInput dadosEntrada) 
+        {
+            // Recuperando contato no banco pelo 'Telefone'.
+            var contato = await _contatoRepositorio.ObterContatoPeloNumeroTelefone(dadosEntrada.Telefone);
+            if (contato == null)
+                return NotFound("Contato não encontrado.");
+
+            contato.Nome = dadosEntrada.Nome;
+            contato.Apelido = dadosEntrada.Apelido;
+            contato.Telefone = dadosEntrada.Telefone;
+            contato.Email = dadosEntrada.Email;
+
+            // Testando a atualização do contato.
+            var contatoAtualizado = await _contatoRepositorio.AtualizarContatos(contato);
+            if (contatoAtualizado)
+                return Ok(contato);
+            return StatusCode(500, "Contato não atualizado.");
+
+        }
+
+        [HttpPut("atualizar-contato-por-id")]
+        public async Task<IActionResult> AtualizarPorId(AtualizarContatoInput dadosEntrada)
+        {
+            // Recuperando contato no banco pelo 'Id'.
+            var contato = await _contatoRepositorio.ObterContatoPeloNumeroId(dadosEntrada.Id);
+            if (contato == null)
+                return NotFound("Contato não encontrado.");
+
+            contato.Nome = dadosEntrada.Nome;
+            contato.Apelido = dadosEntrada.Apelido;
+            contato.Telefone = dadosEntrada.Telefone;
+            contato.Email = dadosEntrada.Email;
+
+            // Testando a atualização do contato.
+            var contatoAtualizado = await _contatoRepositorio.AtualizarContatos(contato);
+            if (contatoAtualizado)
+                return Ok(contato);
+            return StatusCode(500, "Contato não atualizado.");
+
+        }
+
+        [HttpDelete("deletar-contato")]
+        public async Task<IActionResult> Deletar(int id) 
+        {
+            // Recuperando contato no banco pelo 'Id'.
+            var contato = await _contatoRepositorio.ObterContatoPeloNumeroId(id);
+            if (contato == null)
+                return NotFound("Contato não encontrado.");
+
+            var contatoRemovido = await _contatoRepositorio.RemoverContatos(id);
+            if (contatoRemovido)
+                return Ok("Contato removido com sucesso!");
+            return StatusCode(500, "Contato não pode ser removido.");
+        }
 
     }
 }
--- modulo4/projetos/AgendaComDapper/Repositorio/ContatoRepositorio.cs
+++ modulo4/projetos/AgendaComDapper/Repositorio/ContatoRepositorio.cs
@@ -1,5 +1,6 @@
 ﻿using AgendaComDapper.Models;
 using Dapper;
+using Dapper.Contrib.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -18,29 +19,62 @@ namespace AgendaComDapper.Repositorio
             _dbConnection = dbConnection;
         }
 
+        // Obter contato por 'Id'.
+
+        public async Task<Contato> ObterContatoPeloNumeroId(int id)
+        {
+            /* Os comandos abaixo foram comentados para a utilização dos recursos do
+            Dapper.Contrib
+            */
+            /*
+            var comandoSql = "SELECT * FROM contatos WHERE Id = @id";
+            var contato = await _dbConnection.QueryFirstOrDefaultAsync<Contato>(comandoSql,
+                                                          new
+                                                          {
+                                                              id
+                                                          });
+            return contato;
+            */
+            // Através da Biblioteca do Dapper, o 'SELECT * FROM contatos WHERE Id = @id'
+            // foi montado com o comando 'GetAsync'. Performance igual com código menor.
+            var contato = await _dbConnection.GetAsync<Contato>(id);
+            return contato;
+        }
+
         // Listar contatos por telefone.
         public async Task<Contato> ObterContatoPeloNumeroTelefone(string telefone)
         {
-            var comandoSql = "SELECT * FROM contatos WHERE telefone = @telefone";
+                        var comandoSql = "SELECT * FROM contatos WHERE telefone = @telefone";
             var contato = await _dbConnection.QueryFirstOrDefaultAsync<Contato>(comandoSql,
                                                           new
                                                           {
                                                               telefone
                                                           });
-            return contato;
+            return contato;            
         }
 
         // Listar todos os contatos.
         public async Task<IList<Contato>> ListarContatos() 
         {
+            /* Os comandos abaixo foram comentados para a utilização dos recursos do
+            Dapper.Contrib
+            */
+            /*
             var comandoSql = "SELECT * FROM contatos";
             var contatos = await _dbConnection.QueryAsync<Contato>(comandoSql);
+            */
+
+            var contatos = await _dbConnection.GetAllAsync<Contato>();
             return contatos.ToList();
         }
 
         // Adicionar contatos.
         public async Task<bool> AdicionarContatos(Contato contato)
         {
+            /* Os comandos abaixo foram comentados para a utilização dos recursos do
+            Dapper.Contrib
+            */
+            /*
             var comandoSql = "INSERT INTO contatos (Nome,Apelido,Telefone,Email) VALUES (@nome,@apelido,@telefone,@email)";
             var result = await _dbConnection.ExecuteAsync(comandoSql,
                                                           new 
@@ -50,13 +84,20 @@ namespace AgendaComDapper.Repositorio
                                                                telefone = contato.Telefone,
                                                                email = contato.Email
                                                           });
+            */
+
+            var result = await _dbConnection.InsertAsync(contato);
             return result > 0;
         }
 
         // Atualizar contatos.
         public async Task<bool> AtualizarContatos(Contato contato) 
         {
-            var comandoSql = "UPDATE TO contatos SET Nome = @nome, Telefone = @telefone, Email = @email WHERE Id = @id)";
+            /* Os comandos abaixo foram comentados para a utilização dos recursos do
+            Dapper.Contrib
+            */
+            /*
+            var comandoSql = "UPDATE contatos SET Nome = @nome, Telefone = @telefone, Email = @email WHERE Id = @id";
             var result = await _dbConnection.ExecuteAsync(comandoSql,
                                                           new
                                                           {
@@ -66,18 +107,26 @@ namespace AgendaComDapper.Repositorio
                                                               id = contato.Id
                                                           });
             return result > 0;
+            */
+            return await _dbConnection.UpdateAsync(contato);
         }
 
         // Remover contatos.
         public async Task<bool> RemoverContatos(int Idcontato)
         {
-            var comandoSql = "DELETE FROM contatos WHERE Id = @id)";
+            /* Os comandos abaixo foram comentados para a utilização dos recursos do
+            Dapper.Contrib
+            */
+            /*
+            var comandoSql = "DELETE FROM contatos WHERE Id = @id";
             var result = await _dbConnection.ExecuteAsync(comandoSql,
                                                           new
                                                           {                                                              
                                                               id = Idcontato
                                                           });
             return result > 0;
+            */
+            return await _dbConnection.DeleteAsync(new Contato() { Id = Idcontato });
         }
     }
 }
